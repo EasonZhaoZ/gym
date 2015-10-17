d0c6cbf9a75a11c3479366fe13e1b9758cc4b043
EasonZhaoZ <everyday.zy@gmail.com>
2015-10-18 00:14:26 +0800

    compile ok version

--- hadoop/join/src/main/java/com/nowcoder/course/Join.java
+++ hadoop/join/src/main/java/com/nowcoder/course/Join.java
@@ -19,6 +19,7 @@ package com.nowcoder.course;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -28,62 +29,105 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Join {
 
-  public static class TokenizerMapper 
-       extends Mapper<Object, Text, Text, IntWritable>{
+  public static String CTRL_A = "\u0001";
+
+  public static class MapperOne 
+       extends Mapper<Object, Text, IntWritable, Text>{
     
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+    private String tag = "0"; 
+    private IntWritable k = new IntWritable();
+    private Text v = new Text();
      
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
+      String[] vals = value.toString().split("\t", -1);
+      if(2 != vals.length) {
+        context.getCounter("Error", "Input Data Error").increment(1);
+        return;
       }
+      k.set(Integer.valueOf(vals[0]));
+      v.set(tag + CTRL_A + vals[1]);
+      context.write(k, v);
     }
   }
   
-  public static class IntSumReducer 
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+  public static class MapperTwo
+       extends Mapper<Object, Text, IntWritable, Text>{
+    
+    private String tag = "1"; 
+    private IntWritable k = new IntWritable();
+    private Text v = new Text();
+     
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      String[] vals = value.toString().split("\t", -1);
+      if(2 != vals.length) {
+        context.getCounter("Error", "Input data error").increment(1);
+        return;
+      }
+      k.set(Integer.valueOf(vals[0]));
+      v.set(tag + CTRL_A + vals[1]);
+      context.write(k, v);
+    }
+  }
 
-    public void reduce(Text key, Iterable<IntWritable> values, 
+  public static class Reduce 
+       extends Reducer<IntWritable,Text,IntWritable,Text> {
+    
+    private ArrayList<String> dataFromOne = new ArrayList();
+    private ArrayList<String> dataFromTwo = new ArrayList();
+    private Text v = new Text();
+
+    public void reduce(IntWritable key, Iterable<Text> values, 
                        Context context
                        ) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
+      dataFromOne.clear();
+      dataFromTwo.clear();
+      for (Text val : values) {
+        String[] vals = val.toString().split(CTRL_A, -1);
+        if(2 != vals.length) {
+          context.getCounter("Error", "Reduce input error").increment(1);
+          return;
+        }
+        if(vals[0].equals("0")) {
+          dataFromOne.add(vals[1]);
+        } else if(vals[0].equals("1")) {
+          dataFromTwo.add(vals[1]);
+        }
       }
-      result.set(sum);
-      context.write(key, result);
+      // output
+      for(String v1 : dataFromOne) {
+        for(String v2 : dataFromTwo) {
+          v.set(v1 + "\t" + v2);
+          context.write(key, v);
+        }
+      }
     }
   }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-    if (otherArgs.length < 2) {
+    if (otherArgs.length < 3) {
       System.err.println("Usage: wordcount <in> [<in>...] <out>");
       System.exit(2);
     }
-    Job job = Job.getInstance(conf, "word count");
+    Job job = Job.getInstance(conf, "join");
     job.setJarByClass(Join.class);
-    job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    for (int i = 0; i < otherArgs.length - 1; ++i) {
-      FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
-    }
-    FileOutputFormat.setOutputPath(job,
-      new Path(otherArgs[otherArgs.length - 1]));
+    //job.setMapperClass(TokenizerMapper.class);
+    job.setReducerClass(Reduce.class);
+    job.setOutputKeyClass(IntWritable.class);
+    job.setOutputValueClass(Text.class);
+    MultipleInputs.addInputPath(job, new Path(otherArgs[0]), TextInputFormat.class, MapperOne.class);
+    MultipleInputs.addInputPath(job, new Path(otherArgs[1]), TextInputFormat.class, MapperTwo.class);
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
