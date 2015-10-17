0de205aa9258c9fc504a098a1f04e5ea9fb2b823
EasonZhaoZ <everyday.zy@gmail.com>
2015-10-17 23:25:48 +0800

    fix error

--- hadoop/wordcount/run.sh
+++ hadoop/wordcount/run.sh
@@ -5,7 +5,7 @@ set -x
 mvn package -Dmaven.test.skip=true -f pom.xml
 [ $? -ne 0 ] && echo "compile error"
 
-rm -rf output
+rm -rf data/output
 hadoop jar target/wordcount.jar com.nowcoder.course.WordCount data/input data/output
 [ $? -ne 0 ] && echo "excecute error"
 
