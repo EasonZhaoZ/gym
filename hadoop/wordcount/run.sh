c0cde981733a81d370b938a5126b9b216b24a311
EasonZhaoZ <everyday.zy@gmail.com>
2015-10-17 21:32:35 +0800

    add wordcount

---
+++ hadoop/wordcount/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -x
+
+mvn package -Dmaven.test.skip=true -f pom.xml
+[ $? -ne 0 ] && echo "compile error"
+
+rm -rf output
+hadoop jar target/wordcount.jar com.nowcoder.course.WordCount data/input data/output
+[ $? -ne 0 ] && echo "excecute error"
+
+exit 0
