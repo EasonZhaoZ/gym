0b6ab6bccc90d978055cd39756ce5bb568a1d894
EasonZhaoZ <everyday.zy@gmail.com>
2015-10-18 00:15:57 +0800

    join ok version

--- hadoop/join/run.sh
+++ hadoop/join/run.sh
@@ -5,10 +5,8 @@ set -x
 mvn package -Dmaven.test.skip=true -f pom.xml
 [ $? -ne 0 ] && echo "compile error"
 
-exit 0
-
 rm -rf data/output
-hadoop jar target/wordcount.jar com.nowcoder.course.WordCount data/input data/output
+hadoop jar target/join.jar com.nowcoder.course.Join data/input1 data/input2 data/output
 [ $? -ne 0 ] && echo "excecute error"
 
 exit 0
